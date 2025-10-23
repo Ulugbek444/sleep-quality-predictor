db752576e9e3db97b7f0dcb3db85cd0f3352cb9f
Улугбек Турсункулов <blackboy444@mail.ru>
2025-10-24 01:35:25 +0200

    Обновление проекта: Обновлено config для tg_bot

--- tg_bot/config/token.py
+++ tg_bot/config/token.py
@@ -1,4 +1,11 @@
+from dotenv import load_dotenv
 import os
+
+# Поднимаемся на два уровня вверх до корня проекта
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+ENV_PATH = os.path.join(BASE_DIR, ".env")
+
+load_dotenv(ENV_PATH)
+
 API_TOKEN = os.getenv("BOT_TOKEN")
 FASTAPI_URL = os.getenv("FASTAPI_URL")
-
